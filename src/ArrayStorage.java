89b4dae18832bd325fac52361a1093cb8cdaddf8
Дмитрий Васильев <dmitriy.vass@softservice.org>
2021-12-03 19:38:10 +0700

    HW1: implement save, get, delete, clear, getAll, size methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int lastIndex = 0;
 
     void clear() {
+        for (int i = 0; i <= lastIndex; i++) {
+            storage[i] = null;
+            lastIndex = 0;
+        }
     }
 
     void save(Resume r) {
+        storage[lastIndex] = r;
+        lastIndex++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < lastIndex; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < lastIndex; i++) {
+            if (uuid.equals(storage[i].toString())) {
+
+                Resume[] newStorage = new Resume[10000];
+                int remainingElements = size() - ( i + 1 );
+                System.arraycopy(storage, 0, newStorage, 0, i);
+                System.arraycopy(storage, i + 1, newStorage, i, remainingElements);
+                storage = newStorage;
+                lastIndex--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = Arrays.copyOfRange(storage, 0, lastIndex);
+        return newStorage;
     }
 
     int size() {
-        return 0;
+        return lastIndex;
     }
 }
