6ceef42883228989e247bdb148b37039711fb8cd
Дмитрий Васильев <dmitriy.vass@softservice.org>
2021-12-07 15:47:48 +0700

    HW2: change my code into lector's code

--- src/com/javaops/webapp/model/Resume.java
+++ src/com/javaops/webapp/model/Resume.java
@@ -5,7 +5,8 @@ import java.util.Objects;
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable<Resume>{
+
     // Unique identifier
     private String uuid;
 
@@ -18,20 +19,27 @@ public class Resume {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
     public String toString() {
         return uuid;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Resume resume = (Resume) o;
-        return Objects.equals(uuid, resume.uuid);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(uuid);
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
--- src/com/javaops/webapp/storage/ArrayStorage.java
+++ src/com/javaops/webapp/storage/ArrayStorage.java
@@ -7,75 +7,51 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private final int MAX_SIZE = 10_000;
-    private Resume[] storage = new Resume[MAX_SIZE];
-    private int lastIndex = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
-        Arrays.fill(storage, 0, lastIndex, null);
-        lastIndex = 0;
-        System.out.println("Successfully clear storage");
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    public void save(Resume resume) {
-        if (lastIndex == MAX_SIZE) {
-            System.out.println("Failed to save: storage is full");
-            return;
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
         }
-        if (getIndex(resume.getUuid()) != -1) {
-            System.out.println("Failed to save: resume " + resume.toString() + " already exist");
-            return;
-        }
-        storage[lastIndex] = resume;
-        lastIndex++;
-        System.out.println("Successfully save: resume " + resume.toString());
     }
 
-    public Resume get(String uuid) {
-        int resumeIndex = getIndex(uuid);
-        if (resumeIndex != -1) {
-            return storage[resumeIndex];
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
         }
-        return null;
     }
 
     public void delete(String uuid) {
-        int resumeIndex = getIndex(uuid);
-        if (resumeIndex != -1) {
-            storage[resumeIndex] = storage[lastIndex - 1];
-            storage[lastIndex - 1] = null;
-            lastIndex--;
-            System.out.println("Successfully delete: resume " + uuid);
-            return;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-        System.out.println("Failed to delete: resume " + uuid + " not found");
     }
 
-    public void update(Resume resume) {
-        int resumeIndex = getIndex(resume.getUuid());
-        if (resumeIndex != -1) {
-            storage[resumeIndex] = resume;
-            System.out.println("Successfully update resume " + resume.toString());
-            return;
-        }
-        System.out.println("Failed to update: resume " + resume.toString() + " not found");
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, lastIndex);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public int size() {
-        return lastIndex;
-    }
-
-    private int getIndex(String uuid) {
-        for (int i = 0; i < lastIndex; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
