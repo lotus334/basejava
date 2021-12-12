d8015333d95cb88222176adf71090f2b61bfe2bd
Дмитрий Васильев <dmitriy.vass@softservice.org>
2021-12-12 17:15:58 +0700

    HW4: add to test final static resume fields and import static fields. Change constructor's access modifiers

--- test/com/javaops/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/javaops/webapp/storage/AbstractArrayStorageTest.java
@@ -7,7 +7,9 @@ import com.javaops.webapp.model.Resume;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 
 public abstract class AbstractArrayStorageTest {
     protected Storage storage;
@@ -15,18 +17,22 @@ public abstract class AbstractArrayStorageTest {
     protected static final String UUID_1 = "uuid1";
     protected static final String UUID_2 = "uuid2";
     protected static final String UUID_3 = "uuid3";
-    protected static final Resume newResume = new Resume("uuid4");
+    protected static final String UUID_4 = "uuid4";
+    protected static final Resume RESUME_1 = new Resume(UUID_1);
+    protected static final Resume RESUME_2 = new Resume(UUID_2);
+    protected static final Resume RESUME_3 = new Resume(UUID_3);
+    protected static final Resume RESUME_4 = new Resume(UUID_4);
 
-    AbstractArrayStorageTest(Storage storage) {
+    protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
 
     @Before
     public void setUpAbstractArrayStorage() throws Exception {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_3));
-        storage.save(new Resume(UUID_2));
+        storage.save(RESUME_1);
+        storage.save(RESUME_3);
+        storage.save(RESUME_2);
     }
 
     @Test
@@ -43,14 +49,13 @@ public abstract class AbstractArrayStorageTest {
     // TO DO when in Resume appears new fields
     @Test
     public void update() throws Exception {
-        Resume resume = new Resume(UUID_3);
-        storage.update(resume);
-        assertSame(resume, storage.get(resume.getUuid()));
+        storage.update(RESUME_3);
+        assertSame(RESUME_3, storage.get(UUID_3));
     }
 
     @Test(expected = NotExistStorageException.class)
     public void updateNotExist() throws Exception {
-        storage.update(newResume);
+        storage.update(RESUME_4);
     }
 
     @Test
@@ -61,14 +66,13 @@ public abstract class AbstractArrayStorageTest {
     @Test
     public void save() throws Exception {
         int sizeBefore = storage.size();
-        storage.save(newResume);
+        storage.save(RESUME_4);
         assertEquals(sizeBefore + 1, storage.size());
     }
 
     @Test(expected = ExistStorageException.class)
     public void saveExist() throws Exception {
-        Resume resume = new Resume("uuid3");
-        storage.save(resume);
+        storage.save(RESUME_3);
     }
 
     @Test(expected = StorageException.class)
@@ -80,7 +84,7 @@ public abstract class AbstractArrayStorageTest {
         } catch (StorageException e) {
             fail("the overflow happened early");
         }
-        storage.save(newResume);
+        storage.save(RESUME_4);
     }
 
     @Test
@@ -92,7 +96,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNotExist() throws Exception {
-        storage.delete("uuid4");
+        storage.delete(UUID_4);
     }
 
     @Test
