ff4ebc704bef7703c76ec4367057f9e2d8dc93c7
Дмитрий Васильев <dmitriy.vass@softservice.org>
2021-12-15 17:54:17 +0700

    HW5: revert MainTestArrayStorage

--- src/com/javaops/webapp/MainTestArrayStorage.java
+++ src/com/javaops/webapp/MainTestArrayStorage.java
@@ -1,7 +1,6 @@
 package com.javaops.webapp;
 
 import com.javaops.webapp.model.Resume;
-import com.javaops.webapp.storage.ListStorage;
 import com.javaops.webapp.storage.SortedArrayStorage;
 
 /**
@@ -9,57 +8,34 @@ import com.javaops.webapp.storage.SortedArrayStorage;
  */
 public class MainTestArrayStorage {
     static final SortedArrayStorage ARRAY_STORAGE = new SortedArrayStorage();
-    static final ListStorage LIST_STORAGE = new ListStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume("uuid1");
         Resume r2 = new Resume("uuid2");
         Resume r3 = new Resume("uuid3");
         Resume r4 = new Resume("uuid3");
-//
+
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r3);
         ARRAY_STORAGE.save(r2);
-        LIST_STORAGE.save(r1);
-        LIST_STORAGE.save(r3);
-        LIST_STORAGE.save(r2);
 
-//
-//        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
-//        System.out.println("Size: " + ARRAY_STORAGE.size());
-//
-////        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
-//
-//        printAll();
-//        ARRAY_STORAGE.delete(r1.getUuid());
-//        printAll();
-//        ARRAY_STORAGE.delete(r3.getUuid());
-//        printAll();
-//
-////        ARRAY_STORAGE.update(r4);
-//
-//        ARRAY_STORAGE.clear();
-//        printAll();
-//
-//        System.out.println("Size: " + ARRAY_STORAGE.size());
-
-        System.out.println("Get r1: " + LIST_STORAGE.get(r1.getUuid()));
-        System.out.println("Size: " + LIST_STORAGE.size());
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
 
 //        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        LIST_STORAGE.delete(r1.getUuid());
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
-        LIST_STORAGE.delete(r3.getUuid());
+        ARRAY_STORAGE.delete(r3.getUuid());
         printAll();
 
 //        ARRAY_STORAGE.update(r4);
 
-        LIST_STORAGE.clear();
+        ARRAY_STORAGE.clear();
         printAll();
 
-        System.out.println("Size: " + LIST_STORAGE.size());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
