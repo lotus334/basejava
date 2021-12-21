39afa0fe4235ea5b94b4bc5002aa349ea6b0184b
Дмитрий Васильев <dmitriy.vass@softservice.org>
2021-12-21 08:31:59 +0700

    HW6: review: update tests

--- test/com/javaops/webapp/storage/AbstractStorageTest.java
+++ test/com/javaops/webapp/storage/AbstractStorageTest.java
@@ -16,9 +16,9 @@ public abstract class AbstractStorageTest {
     protected static final String UUID_2 = "uuid2";
     protected static final String UUID_3 = "uuid3";
     protected static final String UUID_4 = "uuid4";
-    protected static final Resume RESUME_1 = new Resume("fullName1", UUID_1);
-    protected static final Resume RESUME_2 = new Resume("fullName2", UUID_2);
-    protected static final Resume RESUME_3 = new Resume("fullName3", UUID_3);
+    protected static final Resume RESUME_1 = new Resume("fullNameC", UUID_1);
+    protected static final Resume RESUME_2 = new Resume("fullNameB", UUID_2);
+    protected static final Resume RESUME_3 = new Resume("fullNameA", UUID_3);
     protected static final Resume RESUME_4 = new Resume("fullName4", UUID_4);
 
     protected Storage storage;
@@ -98,7 +98,7 @@ public abstract class AbstractStorageTest {
 
     @Test
     public void getAllSorted() {
-        List<Resume> expectedResumes = List.of(RESUME_1, RESUME_2, RESUME_3);
+        List<Resume> expectedResumes = List.of(RESUME_3, RESUME_2, RESUME_1);
         assertEquals(expectedResumes, storage.getAllSorted());
     }
 
