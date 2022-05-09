02c1f4772375124ee4e9b1c827ac5da0db0a894e
Васильев Дмитрий <lotus33497@yandex.ru>
2022-05-09 11:52:17 +0700

    CW13: add sqlStorage

--- src/com/javaops/webapp/exception/StorageException.java
+++ src/com/javaops/webapp/exception/StorageException.java
@@ -3,6 +3,10 @@ package com.javaops.webapp.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
